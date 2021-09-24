e3c4e4b43d21d1a888283ef549ab030a08318c7e
joshua Owens <owens.joshua98@outlook.com>
2021-09-24 15:48:56 -0500

    added MadLib.cs

---
+++ CFS1Homework/Fruitloop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFS1Homework
+{
+    class Fruitloop
+    {
+        static void Main(string[] args)
+        {
+
+            string[] listOfFruit = { "apple", "pear", "banna", "cherry", "lime", "orange" };
+            for (int i = 1; i < listOfFruit.Length; i++)
+            {
+                Console.WriteLine($"{listOfFruit}");
+            }
+
+
+
+
+        }//main
+    }//class
+}//namespace
---
+++ CFS1Homework/MadLib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFS1Homework
+{
+    class MadLib
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a color");
+            string usercolor = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Enter a car");
+            string usercar = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Enter a book");
+            string userbook = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"You entered {usercolor},{usercar},{userbook}");
+
+
+
+        }//main
+    }//class
+}//namespace
